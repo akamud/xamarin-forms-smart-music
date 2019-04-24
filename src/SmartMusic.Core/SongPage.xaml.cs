8c352a3a1109fbc259f9a06ee66ecff49cf67fe5
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2019-04-24 20:25:36 -0300

    Initial commit

---
+++ src/SmartMusic.Core/SongPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SmartMusic.Core
+{
+    public partial class SongPage : ContentPage
+    {
+        public SongPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
