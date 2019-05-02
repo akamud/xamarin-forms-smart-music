0a43a96c293e25006c0c706985605975c7139f68
Mahmoud Ali <muddibr@gmail.com>
2019-05-02 20:06:20 -0300

    Day 2

---
+++ src/SmartMusic.Core/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SmartMusic
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Song> Songs { get; set; }
+        public MainPageViewModel()
+        {
+            Songs = new ObservableCollection<Song>()
+            {
+                new Song
+                {
+                   Name = "You are as beautiful as spring",
+                   Album = "XIAPI",
+                   OnFire = true,
+                   Position = 1
+                }
+            };
+        }
+    }
+}
---
+++ src/SmartMusic.Core/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartMusic
+{
+    public class Song
+    {
+        public string Name { get; set; }
+        public string Album { get; set; }
+        public bool OnFire { get; set; }
+        public int Position { get; set; }
+    }
+}
